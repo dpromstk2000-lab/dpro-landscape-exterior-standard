e2562e1888a634de8d8096c3490cc001f5c1fbf0
dpromstk2000-lab <dpromstk2000@gmail.com>
2026-09-03 10:43:06 +0900

    Bind final standard production template URLs

--- site/config.production.template.js
+++ site/config.production.template.js
@@ -1,7 +1,7 @@
 window.DPRO_LANDSCAPE_CONFIG = Object.freeze({
   systemId: 'dpro_landscape_exterior',
   version: '1.0.0-g7-shared-bind',
-  apiBase: 'https://__WORKER_HOST__/api/landscape/v1',
+  apiBase: 'https://dpro-landscape-exterior-core.dpromstk2000.workers.dev/api/landscape/v1',
   demo: false,
   supabaseUrl: 'https://cbknucemarcpbscirzyv.supabase.co',
   supabasePublishableKey: 'sb_publishable_cT4vj0FoU1MtjFayuk7HVw_YcuSMR5J',
